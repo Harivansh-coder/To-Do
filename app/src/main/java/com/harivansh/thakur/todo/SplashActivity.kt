963c656a4991c765b57a29106b803f53964891f1
Harivansh Thakur <harivanshthakur8@gmail.com>
2021-12-05 16:16:07 +0530

    First commit

---
+++ app/src/main/java/com/harivansh/thakur/todo/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.harivansh.thakur.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGTH: Long = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed(Runnable {
+            val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            this@SplashActivity.startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH)
+
+    }
+}
