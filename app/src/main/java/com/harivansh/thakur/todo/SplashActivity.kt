810f2d27c2d82162956c0a6c811162418b146a8f
Harivansh Thakur <harivanshthakur8@gmail.com>
2021-12-07 22:25:46 +0530

    Added the logo on the splash screen

--- app/src/main/java/com/harivansh/thakur/todo/SplashActivity.kt
+++ app/src/main/java/com/harivansh/thakur/todo/SplashActivity.kt
@@ -3,24 +3,31 @@ package com.harivansh.thakur.todo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-import android.os.Handler
+
 
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_DISPLAY_LENGTH: Long = 1000
+//    private val SPLASH_DISPLAY_LENGTH: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        super.onCreate(savedInstanceState);
 
-        supportActionBar?.hide()
 
-        Handler().postDelayed(Runnable {
-            val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
-            this@SplashActivity.startActivity(mainIntent)
-            finish()
-        }, SPLASH_DISPLAY_LENGTH)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
+        //super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_splash)
+//
+//        supportActionBar?.hide()
+//
+//        Handler().postDelayed(Runnable {
+//            val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
+//            this@SplashActivity.startActivity(mainIntent)
+//            finish()
+//        }, SPLASH_DISPLAY_LENGTH)
 
     }
 }
