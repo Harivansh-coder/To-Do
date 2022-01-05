8a642da19cd0237a3324c26336e466fd9ba2408b
Harivansh Thakur <harivanshthakur8@gmail.com>
2022-01-05 22:45:31 +0530

    added icons and model class

--- app/src/main/java/com/example/todo/model/Task.kt
+++ app/src/main/java/com/example/todo/model/Task.kt
@@ -1,34 +1,4 @@
 package com.example.todo.model
 
-import android.widget.Toast
 
-class Task {
-
-    var taskName: String? = null
-    var taskStatus: Boolean? = null
-
-    fun temp() {}
-
-    fun temp(taskName: String?, taskStatus: Boolean?) {
-        this.taskName = taskName
-        this.taskStatus = taskStatus
-    }
-
-//    fun getTaskName(): String? {
-//        return taskName
-//    }
-//
-//    fun setTaskName(taskName: String?) {
-//        this.taskName = taskName
-//    }
-//
-//    fun getTaskStatus(): Boolean? {
-//        return taskStatus
-//    }
-//
-//    fun setTaskStatus(taskStatus: Boolean?) {
-//        this.taskStatus = taskStatus
-//    }
-
-
-}
+class Task(taskName: String , taskDesc : String? = null , taskStatus: Boolean)
